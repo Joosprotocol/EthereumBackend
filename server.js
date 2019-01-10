5d780d3d4965af8038a1e1321eb4c541abd04dc6
Retriever Cobalt <duxtarr@gmail.com>
2019-01-10 08:20:45 +0000

    ENH: Add web3 custom call (pull request #4)
    * Add web3 custom call.
    
    * Change contract param check.
    
    Approved-by: Artem Hryhorenko <dev7it@gmail.com>

--- server.js
+++ server.js
@@ -15,12 +15,14 @@ let private_key = Buffer.from(config.private_key, 'hex');
 
 const STORAGE = config.storage;
 
-REQUEST_TYPE_CALL = 'call';
-REQUEST_TYPE_SEND = 'send';
+REQUEST_TYPE_CALL_CONTRACT = 'call-contract';
+REQUEST_TYPE_SEND_CONTRACT = 'send-contract';
+REQUEST_TYPE_CALL_WEB3_CUSTOM = 'call-web3-custom';
 
 const REQUEST_TYPE = [
-    REQUEST_TYPE_CALL,
-    REQUEST_TYPE_SEND
+    REQUEST_TYPE_CALL_CONTRACT,
+    REQUEST_TYPE_SEND_CONTRACT,
+    REQUEST_TYPE_CALL_WEB3_CUSTOM,
 ];
 
 const logger = winston.createLogger({
@@ -42,10 +44,14 @@ app.post('/', async (req, res) => {
         checkBody(req.body);
 
 
-        if (req.body.type === REQUEST_TYPE_CALL) {
+        if (req.body.type === REQUEST_TYPE_CALL_CONTRACT) {
             response = await callContract(req.body.contract_name, req.body.method, req.body.params);
-        } else {
+        }
+        if (req.body.type === REQUEST_TYPE_SEND_CONTRACT) {
             response = await sendContract(req.body.contract_name, req.body.method, req.body.params);
+        }
+        if (req.body.type === REQUEST_TYPE_CALL_WEB3_CUSTOM) {
+            response = await callWeb3Custom(req.body.method, req.body.params);
         }
 
         output = {
@@ -78,10 +84,6 @@ function UserException(message) {
 }
 
 function checkBody(body) {
-    if (body.contract_name === undefined || STORAGE[body.contract_name] === undefined) {
-        throw new UserException("Missing or invalid parameter 'contract_name'.");
-    }
-
     if (body.type === undefined || REQUEST_TYPE.indexOf(body.type) === -1) {
         throw  new UserException("Missing or invalid parameter 'type'. Available: call, send.");
     }
@@ -97,7 +99,27 @@ function checkBody(body) {
     return true;
 }
 
+function callWeb3Custom(method, params) {
+    let context = web3;
+    var namespaces = method.split(".");
+    for(var i = 0; i < namespaces.length; i++) {
+        context = context[namespaces[i]];
+    }
+    return context(...params);
+}
+
+function isContractDefine(contract_name) {
+    if (contract_name === undefined || STORAGE[contract_name] === undefined) {
+        return false;
+    }
+    return true;
+}
+
 function callContract(contract_name, method, params) {
+
+    if (isContractDefine(contract_name)) {
+        throw new UserException("Missing or invalid parameter 'contract_name'.");
+    }
 
     let contract = new web3js.eth.Contract(STORAGE[contract_name].abi, STORAGE[contract_name].address);
 
@@ -108,6 +130,10 @@ function callContract(contract_name, method, params) {
 };
 
 async function sendContract (contract_name, method, params) {
+    if (isContractDefine(contract_name)) {
+        throw new UserException("Missing or invalid parameter 'contract_name'.");
+    }
+
     let v = await web3js.eth.getTransactionCount(my_address);
     let contract = new web3js.eth.Contract(STORAGE[contract_name].abi, STORAGE[contract_name].address);
     let rawTransaction = {
